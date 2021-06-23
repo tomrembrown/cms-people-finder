c7b6871dc2e0195b47fad39680d76e38a7d862e8
Tom Brown <tombrown654321@protonmail.com>
2021-06-23 11:03:16 -0400

    Built scripts to create database tables

--- database/createTables.sql
+++ database/createTables.sql
@@ -7,8 +7,22 @@ Script to create the CMS People Finder database
 --  database as user pf_computer_access
 
 -- Drop tables in reverse order of creation
-DROP TABLE IF EXISTS high_scores;
+DROP TABLE IF EXISTS profiles;
 DROP TABLE IF EXISTS login;
 
+CREATE TABLE login(
+  id SERIAL PRIMARY KEY,
+  email CITEXT NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  signup_date TIMESTAMP NOT NULL
+);
 
+-- This table has same primary key as previous table - so 1 to 1 relation
+-- You could put all of the information in profiles into login
+-- I made a choice to separate them - since profiles has no 'confidential'
+-- information, whereas login does, such as the email and the hashed password
+CREATE TABLE profiles(
+  id INTEGER REFERENCES login(id) PRIMARY KEY,
+  user_handle CITEXT NOT NULL UNIQUE
+);
 
