fdba9bf78ea5a4ddb5ac0c124c0db208b157fda8
Tom Brown <tombrown654321@protonmail.com>
2021-09-12 12:09:12 -0400

    Added more tables for interests, projects, skills to database

--- database/createTables.sql
+++ database/createTables.sql
@@ -7,6 +7,13 @@ Script to create the CMS People Finder database
 --  database as user pf_computer_access
 
 -- Drop tables in reverse order of creation
+DROP TABLE IF EXISTS profiles_projects;
+DROP TABLE IF EXISTS user_types;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS profiles_skills;
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS profiles_interests;
+DROP TABLE IF EXISTS interests;
 DROP TABLE IF EXISTS profiles;
 DROP TABLE IF EXISTS login;
 
@@ -26,7 +33,52 @@ CREATE TABLE profiles(
   handle CITEXT NOT NULL UNIQUE,
   tagline TEXT DEFAULT NULL,
   description TEXT DEFAULT NULL,
-  image_filename TEXT DEFAULT NULL
+  image_filename TEXT DEFAULT NULL,
+  aboutme TEXT DEFAULT NULL,
+  location TEXT DEFAULT NULL
+);
+
+CREATE TABLE interests(
+  id SERIAL PRIMARY KEY,
+  interest CITEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE profiles_interests(
+  profile_id INTEGER REFERENCES login(id) NOT NULL,
+  interest_id INTEGER REFERENCES interests(id) NOT NULL,
+  display_order SMALLINT NOT NULL,
+  PRIMARY KEY(profile_id, interest_id)
+);
+
+CREATE TABLE skills(
+  id SERIAL PRIMARY KEY,
+  skill CITEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE profiles_skills(
+  profile_id INTEGER REFERENCES login(id) NOT NULL,
+  skill_id INTEGER REFERENCES skills(id) NOT NULL,
+  display_order SMALLINT NOT NULL,
+  PRIMARY KEY(profile_id, skill_id)
+);
+
+CREATE TABLE projects(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL UNIQUE,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE user_types(
+  id SERIAL PRIMARY KEY,
+  type CITEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE profiles_projects(
+  profile_id INTEGER REFERENCES login(id) NOT NULL,
+  project_id INTEGER REFERENCES projects(id) NOT NULL,
+  user_type INTEGER REFERENCES user_types(id) NOT NULL,
+  display_order INTEGER NOT NULL,
+  PRIMARY KEY(profile_id, project_id)
 );
 
 -- Create a default user in the profiles table. This is used as defaults for
@@ -37,3 +89,39 @@ INSERT INTO login(id, email, password_hash, signup_date) VALUES
 INSERT INTO profiles(id, handle, tagline, description) VALUES
   (0, 'Toby the Cat', 'TOTALLY AWESOME MANIFESTATION OF INFINITE CONSCIOUSNESS', 
       'As a cat, much of my time is spent either sleeping or manipulating humans into being my servants with my irresitible cuteness. I have found loud whiny noises useful for obtaining food and cuddles. Boxes are useful places to hide in. In the early evening strange lights (reflections from electronic device screens) appear on the ceiling and walls. I have tried communicated with these, but so far have not received a response.');
+
+INSERT INTO interests(interest) VALUES
+  ('Hiking'),
+  ('Backpacking'),
+  ('Camping'),
+  ('Hunting'),
+  ('Fishing'),
+  ('Archery'),
+  ('Canoeing'),
+  ('Kayaking'),
+  ('Running'),
+  ('Geocaching'),
+  ('Growing Vegetables'),
+  ('Composting');
+
+INSERT INTO skills(skill) VALUES
+  ('Conflict Resolution'),
+  ('Communication'),
+  ('Brand Management'),
+  ('Inventory Control'),
+  ('Data Entry'),
+  ('Web Design'),
+  ('Web Development'),
+  ('Database Design'),
+  ('Emotional Intelligence'),
+  ('Collaboration'),
+  ('Artificial Intelligence'),
+  ('Deep Learning'),
+  ('Supply Chain Management'),
+  ('Cloud Computing'),
+  ('UX Design'),
+  ('Video Production');
+
+INSERT INTO user_types(id, type) VALUES
+  (1, 'Lead'),
+  (2, 'Collaborator');
